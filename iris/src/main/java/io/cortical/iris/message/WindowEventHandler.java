d1c1a24e730641bd9d69d33f3855714576648931
cogmission <cogitionmission@gmail.com>
2017-06-06 11:53:21 -0500

    Initialize new project

--- iris/src/main/java/io/cortical/iris/message/WindowEventHandler.java
+++
@@ -1,9 +0,0 @@
-package io.cortical.iris.message;
-
-import io.cortical.iris.window.Window;
-import javafx.event.Event;
-
-@FunctionalInterface
-public interface WindowEventHandler {
-    public abstract void handle(Event e, Window w);
-}
